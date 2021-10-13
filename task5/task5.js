922fba5ef4dc1e0998b00018222333929617a899
Olga Albukova <o.albukova@gmail.com>
2021-10-13 16:44:54 +0300

    task5

--- task5/task5.js
+++ task5/task5.js
@@ -1,20 +1,27 @@
 const headerMenuItem = document.querySelector(".header-menu__item");
 const submenu = document.querySelector(".submenu");
+const header = document.querySelector(".header");
+const headerLogo = document.querySelector(".header__logo");
+const sticky = header.offsetTop;
+
 
 function submenuClassListToggle(elem) {
   if (document.documentElement.clientWidth < 500)
     elem.classList.toggle("submenu_active");
 }
-// }
+
 headerMenuItem.addEventListener("click", () => submenuClassListToggle(submenu));
 
-// document.addEventListener("DOMContentLoaded", (event) => {
-//   headerMenuItem.addEventListener("click", () => submenuClassListToggle(submenu));
+window.onscroll = function () {
+  myFunction();
+};
 
-// });
-
-// window.addEventListener("resize", (event) => {
-//    headerMenuItem.addEventListener("click", () =>
-//      submenuClassListToggle(submenu)
-//    );
-// });
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("header_scroll");
+    headerLogo.classList.add("header__logo_scroll");
+  } else {
+    header.classList.remove("header_scroll");
+    headerLogo.classList.remove("header__logo_scroll");
+  }
+}
